1550f65b0e748678a2409a3a8358afba447658fd
Andrea <andrea.barberio97@gmail.com>
2022-10-30 20:29:03 +0100

    Update: created MyForm Component and initialized form fields

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,8 +1,41 @@
 import * as React from "react";
 import { TextField } from "@mui/material";
-interface Props {
-  onSubmit: () => void;
+import { Formik, Form } from "formik";
+interface Values {
+  userName: string;
+  Password: string;
 }
-export const MyForm: React.FC<Props> = () => {
-  return <TextField />;
+interface Props {
+  onSubmit: (values: Values) => void;
+}
+export const MyForm: React.FC<Props> = ({ onSubmit }) => {
+  return (
+    <Formik
+      initialValues={{ userName: "", Password: "" }}
+      onSubmit={(values) => {
+        onSubmit(values);
+      }}
+    >
+      {({ values, handleChange, handleBlur }) => (
+        <Form>
+          <TextField
+            placeholder="Username"
+            name="userName"
+            value={values.userName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />{" "}
+          {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+          <TextField
+            placeholder="Password"
+            type="password"
+            name="Password"
+            value={values.Password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />{" "}
+        </Form>
+      )}
+    </Formik>
+  );
 };
